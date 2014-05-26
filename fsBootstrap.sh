b5623987ce05a69469d2882491c961fc5fd094b4
John Grub <john.f.grub@gmail.com>
2014-05-25 20:27:33 -0700

    Create fsBootstrap.sh

---
+++ fsBootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+/debootstrap/debootstrap --second-stage --verbose
+
+cat  > /etc/network/interfaces.d/eth0 <<END
+auto eth0
+allow-hotplug eth0
+iface eth0 inet dhcp
+iface eth0 inet6 auto
+END
+
+cat > /etc/hosts << END
+127.0.0.1 localhost
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+END
+
+echo matrix > /etc/hostname
+
+echo 'T0:2345:respawn:/sbin/getty -L ttymxc0 115200 linux' >> /etc/inittab
+sed -i 's/^\([1-6]:.* tty[1-6]\)/#\1/' /etc/inittab
+
+dpkg-reconfigure locales
+dpkg-reconfigure tzdata
+
+cat > /etc/apt/sources.list << END
+deb http://http.debian.net/debian sid main contrib non-free
+deb-src http://http.debian.net/debian sid main contrib non-free
+END
+
+echo "Choose root password:"
+passwd
+
+reboot -f
